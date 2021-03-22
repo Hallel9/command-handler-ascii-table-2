d161a084e3502cc5770c61935d70e763ce332e83
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-03-22 10:09:41 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 const Guild = require('../models/guild');
 const config = require('../config.json')
-
 module.exports = async (client, message) => {
     if (message.author.bot) return;
 
@@ -14,7 +14,7 @@ module.exports = async (client, message) => {
                 _id: mongoose.Types.ObjectId(),
                 guildID: message.guild.id,
                 guildName: message.guild.name,
-                prefix: config.prefix
+                prefix: config.prefix,
             })
 
             newGuild.save()
@@ -28,17 +28,18 @@ module.exports = async (client, message) => {
     const prefix = settings.prefix;
 
     if (!message.guild) return;
-
-    if(message.content === `<@!${client.user.id}>` || message.content === `<@${client.user.id}>`) {
-        return message.channel.send("This works.");
-    };
-
-
-    if (!message.content.startsWith(prefix)) return;
     
     if (!message.member) message.member = await message.guild.fetchMember (message);
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+    const prefixRegex = new RegExp(`^(<@!?${message.client.user.id}>|${escapeRegex(prefix)})\\s*`);
+
+    if(!prefixRegex.test(message.content)) return;
+
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+
+
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
     const cmd = args.shift().toLowerCase();
     
     if (cmd.length === 0) return;
